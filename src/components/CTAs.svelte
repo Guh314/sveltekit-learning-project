7c0c616e1ef22ecd9a9ab732d459877967e9b505
gustavo.rafael <gustavorafael2010@gmail.com>
2024-09-11 09:18:16 -0300

    Progress, still no svelkit specifics...

--- src/components/CTAs.svelte
+++ src/components/CTAs.svelte
@@ -4,7 +4,7 @@
     <button class="specialBtn">
        <p class="text-base sm:text-lg md:text-lg">Contact Us</p> 
     </button>
-    <button class="specialBtnBlack">
+    <button class="specialBtnDark">
        <p class="text-base sm:text-lg md:text-lg">Get Started &rarr;</p> 
     </button>
 </div>
---
+++ src/components/ReviewCard.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let reviewItem;
+    export let left;
+    export let index;
+</script>
+
+
+<div class={"flex flex-col gap-2 w-full max-w-[500px] mx-auto relative " + (
+    left ? index % 2 === 1 ?
+    "hidden md:flex opacity-0 pointer-events-none max-h-[40px] overflow-hidden" : ""
+    : index % 2 === 0 ? 
+    "hidden md:flex opacity-0 pointer-events-none max-h-[40px] overflow-hidden" : ""
+)}>
+    <div class="flex relative">
+        <div class={"absolute rounded-full bg-white aspect-squared hidden md:grid place-items-center h-4 top-0 " + index % 2 === 0 ? "right-0 -mr-8 transalte-x-1/2" : "left-0 -ml-8 -transalte-x-1/2"}>
+            <div class="rounded-full bg-slate-950 aspect-square bg-slate-950 h-2"></div>
+        </div>
+        <div class=""></div>
+    </div>
+</div>
