8236f97c92bb02e0b98f57009d3b6f4a024048e8
gustavo.rafael <gustavorafael2010@gmail.com>
2024-09-12 16:31:13 -0300

    Still no svelte specifics...

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,6 +2,7 @@
     import SectionWrapper from "../components/SectionWrapper.svelte";
     import Header from "../components/Header.svelte";
     import CTAs from "../components/CTAs.svelte";
+    import Stars from "../components/Stars.svelte";
 </script>
 
 <SectionWrapper>
@@ -16,17 +17,6 @@
             Less thinking & more doing. We're the trainer in your pocket full of <span class="italic">personalized workouts, exercises explanations analytics</span> and much much more.
         </p>
         <CTAs/>
-        <div class="flex items-center justify-center gap-2 text-base">
-            <p>4.6</p>
-            {#each [0,1,2,3,4] as index}
-            <div class="grid place-items-center relative">
-                <i class="fa-solid fa-star opacity-0"></i>
-                <div class={"absolute top-0 left-0 grid place-items-center " + (index ===  4 ? "w-[60%] overflow-hidden" : "")}>
-                    <i class="fa-solid fa-star text-amber-400"></i>
-                </div>
-            </div>
-            {/each}
-            <p>500+</p>
-        </div>
+        <Stars />
     </div>
 </SectionWrapper>
--- src/components/ReviewCard.svelte
+++ src/components/ReviewCard.svelte
@@ -13,8 +13,23 @@
 )}>
     <div class="flex relative">
         <div class={"absolute rounded-full bg-white aspect-squared hidden md:grid place-items-center h-4 top-0 " + index % 2 === 0 ? "right-0 -mr-8 transalte-x-1/2" : "left-0 -ml-8 -transalte-x-1/2"}>
-            <div class="rounded-full bg-slate-950 aspect-square bg-slate-950 h-2"></div>
+            <div class="rounded-full bg-slate-950 aspect-square bg-slate-950 h-2" />
         </div>
-        <div class=""></div>
+        <div class="flex items-center gap-2">
+            <i class="fa-solid fa-user" />
+            <h3 class="text-lg sm:text-xl md:text-2xl">
+                {reviewItem.name}
+            </h3>
+        </div>
     </div>
+    <div class="h-[1.5px] bg-slate-950 w-1/4 mr-auto mb-auto mb-4 mt-2">
+    </div>
+    <div class="flex items-center flex-wrap gap-2 text-xs sm:text-sm mb-4">
+        {#each reviewItem.features as keyword}
+            <div class="p-1 py-0.5 rounded-md border border-solid border indigo-400 bg-indigo-50 text-indigo-400">
+                <p>{keyword}</p>
+            </div>
+        {/each}
+    </div>
+    <p>{reviewItem.review}</p>
 </div>
